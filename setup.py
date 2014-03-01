bdacf768159e4bacc414c91d6b0a23c317626eb2
Alex Rudy <alex.rudy@gmail.com>
2014-03-01 13:46:08 -0800

    Fixing setup.py

--- setup.py
+++ setup.py
@@ -7,28 +7,29 @@
 #  Copyright 2012 Alexander Rudy. All rights reserved.
 # 
 
-try:
-    import setuptools
-except ImportError:
-    from distribute_setup import use_setuptools
-    use_setuptools()
 
 from setuptools import setup, find_packages
 
-from pyobserver import version
-
 setup(
     name = "pyobserver",
-    version = version,
+    version = "0.1",
     packages = find_packages(exclude=['tests']),
     package_data = {'pyobserver': ['pyobserver/data/*']},
-    install_requires = ['distribute','astropy>=0.2','pyshell>=0.3.0','pyds9==1.6'],
-    dependency_links = [
-        'https://github.com/alexrudy/pyshell/archive/0.3.0.tar.gz#egg=pyshell-0.3.0',
-        'http://hea-www.harvard.edu/RD/download/pyds9/pyds9-1.6.tar.gz#egg=pyds9-1.6'
-    ],
     author = "Alexander Rudy",
     author_email = "arrudy@ucsc.edu",
+    install_requires = [
+        'Jinja2==2.7.2',
+        'PyYAML==3.10',
+        'astropy==0.3',
+        'numpy==1.8.0',
+        'six==1.5.2',
+        'pyds9==1.6',
+        'pyshell>=0.3.0',
+    ],
+    dependency_links = [
+        'http://hea-www.harvard.edu/RD/download/pyds9/pyds9-1.6.tar.gz#egg=pyds9',
+        'git+https://github.com/alexrudy/pyshell.git@develop#egg=pyshell',
+    ],
     entry_points = {
         'console_scripts' : [
             "PO = pyobserver.cli:POcommand.script"
