4238a5bf7908d358af07568f13bee08b370b1f47
Alex Rudy <alex.rudy@gmail.com>
2014-03-04 10:42:09 -0800

    Better handling of times in night.

--- pyobserver/ephem/visibility.py
+++ pyobserver/ephem/visibility.py
@@ -14,6 +14,7 @@ from __future__ import (absolute_import, unicode_literals, division,
 import numpy as np
 
 import astropy.units as u
+import astropy.time
 
 from .targets import Sun, Moon
 
@@ -59,21 +60,18 @@ class Night(object):
         self._moon.compute(self.observer)
         return self._moon
         
-    def iterate(self, increment):
+    def times(self, increment):
         """Iterate through a night at a given increment, from sunset to sunrise."""
-        time = self.start
-        while time <= self.end:
-            yield time
-            time = time + increment
-            
-    def increments(self, increment):
-        """The number of increments which will occur in this night."""
-        return (self.length/increment) + 1
+        data = []
+        for i in range(int((self.length.to(u.day)/increment).to(1)) + 1):
+            data.append(self.start + increment * i)
+        return astropy.time.Time([ d.datetime for d in data ], scale='utc')
+    
 
 class VisibilityPlot(object):
     """A single observing night at a specific observatory."""
     
-    increment = 6 * u.minute
+    increment = 1 * u.minute
     
     def __init__(self, observer, date):
         super(VisibilityPlot, self).__init__()
@@ -82,23 +80,22 @@ class VisibilityPlot(object):
     
     def __call__(self, ax):
         """Make a visibility plot on a given axes object."""
-        if hasattr(ax, '_twinx_ax'):
-            ax_z = ax._twinx_ax
-        else:
-            ax_z = ax.twinx()
-            ax._twinx_ax = ax_z
+        import matplotlib.dates
+        locator = matplotlib.dates.AutoDateLocator()
+        formatter = matplotlib.dates.AutoDateFormatter(locator)
         
-        n = self.night.increments(self.increment)
-        times = list(self.night.iterate(self.increment))
-        dt = [ time.datetime for time in times ]
+        times = self.night.times(self.increment)
         for target in self.targets:
-            zenith_angle = np.zeros((n,), dtype=float) * u.degree
-            moon_distance = np.zeros((n,), dtype=float) * u.degree
+            altitude_angle = np.zeros((len(times),), dtype=float) * u.degree
+            moon_distance = np.zeros((len(times),), dtype=float) * u.degree
             for i,time in enumerate(times):
                 self.night.observer.date = time
                 target.compute(self.night.observer)
-                zenith_angle[i] = target.alt
+                altitude_angle[i] = target.alt
                 moon_distance[i] = self.night.moon.position.separation(target.position)
-            ax.plot(dt, zenith_angle, '-', label=target.name)
+            ax.plot(times.plot_date, altitude_angle, '-', label=target.name)
+        ax.xaxis.set_major_locator(locator)
+        ax.xaxis.set_major_formatter(formatter)
+        ax.set_ylim(0, 90)
         ax.legend()
         
