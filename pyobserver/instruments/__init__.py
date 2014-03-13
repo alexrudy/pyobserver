25e170629017388be92245596f7717b456a1d46c
Alex Rudy <alex.rudy@gmail.com>
2014-03-13 10:33:09 -0700

    New Instruments module

---
+++ pyobserver/instruments/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# 
+#  __init__.py
+#  pyobserver
+#  
+#  Created by Alexander Rudy on 2014-03-06.
+#  Copyright 2014 University of California. All rights reserved.
+# 
+
+from __future__ import (absolute_import, unicode_literals, division, print_function)
---
+++ pyobserver/instruments/osiris.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# 
+#  osiris.py
+#  pyobserver
+#  
+#  Created by Alexander Rudy on 2014-03-06.
+#  Copyright 2014 University of California. All rights reserved.
+# 
+
+from __future__ import (absolute_import, unicode_literals, division, print_function)
+
+import numpy as np
+import astropy.table
+import astropy.cosmology
+import pkg_resources
+import pyshell.config
+import astropy.units as u
+
+def get_osiris_filters():
+    """Retrieve an astropy table containing the OSIRIS filters."""
+    config = get_osiris_configuration()
+    table = astropy.table.Table.read(pkg_resources.resource_stream('pyobserver','data/{}'.format(config["filters.table"])), format='ascii.tab')
+    return table
+    
+_config = pyshell.config.StructuredConfiguration.fromresource('pyobserver', 'data/osiris_info.yml')
+def get_osiris_configuration():
+    """Get the osiris configuration."""
+    return _config
+
+def osiris_scales_at_redshift(z, cosmo=None):
+    """Return a table of OSIRIS filters and thier respective scales."""
+    if cosmo is None:
+        cosmo = astropy.cosmology.get_current()
+    config = get_osiris_configuration()
+    kpc_per_arcmin = cosmo.kpc_proper_per_arcmin(z)
+    pixel_scales = (np.array(config["scales"]) * u.arcsec)
+    physical_scales = pixel_scales * kpc_per_arcmin
+    return physical_scales, pixel_scales
+    
+
