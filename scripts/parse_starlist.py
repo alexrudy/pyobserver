c0b0cecd3ab2aefe4c1b0f79e80f01996e666077
Alex Rudy <alex.rudy@gmail.com>
2014-03-15 16:39:40 -0700

    Scripts to parse starlists.

--- scripts/parse_starlist.py
+++ scripts/parse_starlist.py
@@ -11,12 +11,13 @@
 from __future__ import (absolute_import, unicode_literals, division, print_function)
 
 from pyobserver.ephem import VisibilityPlot, Target, Observer
-from pyobserver.util import read_skip_comments, parse_starlist, _starlist_re_raw
+from pyobserver.starlist import read_skip_comments, parse_starlist, _starlist_re_raw, verify_starlist_line, parse_starlist_line
 
 from pyshell.util import ipydb
 ipydb()
 
 import sys
+import os, os.path
 
 import re
 
@@ -28,13 +29,19 @@ test_parts_A = [
     "lgs=1 skip=3"
 ]
 test_parts_B = [
-    "PG 0026+129      ",
-    "00 29 13.7000  ",
+    "PG 0026+129     ",
+    "00 29 13.7000 ",
     "13 16 03.720 ",
     "2000 ",
     "lgs=1 skip=3"
 ]
-    
+test_parts_C = [
+    "PG 0026+129      ",
+    "00 29 13.7000  ",
+    "13 16 03.720 ",
+    "2000 ",
+    "lgs=1 skip=3 bad==2=2 badkeyword"
+]
 
 def test_parse_starlist_tokens(test_parts):
     """Test parsing a set of tokens from a starlist."""
@@ -48,19 +55,27 @@ def test_parse_starlist_tokens(test_parts):
         else:
             print("Failed to parse!")
         print("")
+        
+def test_verify_starlist_tokens(test_parts):
+    """Test the verification of starlist tokens."""
+    
+    print("Verifying Line:")
+    print("".join(test_parts))
+    verify_starlist_line("".join(test_parts))
 
 if __name__ == '__main__':
     if not len(sys.argv) == 2:
-        test_parse_starlist_tokens(test_parts_A)
-        test_parse_starlist_tokens(test_parts_B)
+        # test_parse_starlist_tokens(test_parts_A)
+        test_verify_starlist_tokens(test_parts_A)
+        # test_parse_starlist_tokens(test_parts_B)
+        test_verify_starlist_tokens(test_parts_B)
+        test_verify_starlist_tokens(test_parts_C)
         sys.exit(0)
     
     filename = sys.argv[1]
     
-    print("Parsing as PyEphem Targets...")
-    for target_line in read_skip_comments(filename):
-        print(Target.from_starlist(target_line))
+    print("Parsing starlist...")
+    for line_no, target_line in enumerate(read_skip_comments(filename)):
+        verify_starlist_line(target_line, "'{0}' line {1:d}".format(os.path.basename(filename), line_no))
+        print(parse_starlist_line(target_line))
      
-    print("Parsing as data dictionaries...")   
-    for data in parse_starlist(filename):
-        print(data)
