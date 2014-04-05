c32c5ea20acded43342906ff818de3a3089ec3a5
Alex Rudy <alex.rudy@gmail.com>
2014-04-05 11:06:52 -0700

    Base class for observatories

---
+++ pyobserver/observatory/instrument.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# 
+#  instrument.py
+#  pyobserver
+#  
+#  Created by Alexander Rudy on 2014-04-04.
+#  Copyright 2014 Alexander Rudy. All rights reserved.
+# 
+
+from __future__ import (absolute_import, unicode_literals, division, print_function)
+
+
