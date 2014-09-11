ac129fb386bb3794d033d5299d12a07bd54379dc
Alex Rudy <alex.rudy@gmail.com>
2014-09-10 17:20:12 -0700

    Targets using a Starlist base

--- pyobserver/visibility/targets.py
+++ pyobserver/visibility/targets.py
@@ -11,10 +11,16 @@ from __future__ import (absolute_import, unicode_literals, division, print_funct
 
 
 from astropyephem import FixedBody
+from astropy.coordinates import AltAz, FK5, FK4
 from ..starlist import parse_starlist_line, read_skip_comments
 
-class Target(FixedBody):
-    """A subclass of FixedBody with a starlist interface."""
+class StarlistBase(object):
+    """Base class for starlist."""
+    
+    REGISTRY = {}
+    
+    def __init__(self, *args, **kwargs):
+        super(StarlistBase, self).__init__()
     
     @classmethod
     def from_starlist(cls, text):
@@ -22,7 +28,68 @@ class Target(FixedBody):
         data = parse_starlist_line(text)
         for key in data:
             data[key.lower()] = data.pop(key)
-        return cls(**data)
+        obj = None
+        klass = cls.REGISTRY[type(data['position'])]
+        return klass(**data)
+    
+    @classmethod
+    def register(cls, coords):
+        """Register a superclass to this constructor."""
+        def _register(klass):
+            cls.REGISTRY[coords] = klass
+            return klass
+        return _register
+
+@StarlistBase.register(AltAz)
+class AltAzTarget(StarlistBase):
+    """Altitude and azimuthal target."""
+    
+    def __init__(self, name, **kwargs):
+        super(AltAzTarget, self).__init__()
+        self.name = name
+        for key in kwargs:
+            setattr(self, key, kwargs[key])
+    
+    def position_string(self):
+        """Return a string representation of the position"""
+        return "Alt={alt},Az={az}".format(
+            alt = self.position.alt,
+            az = self.position.az,
+        )
+    
+    def __repr__(self):
+        """Represent this object"""
+        repr_str = "<{} ".format(self.__class__.__name__)
+        if self.name is not None:
+            repr_str += "'{}'".format(self.name)
+        try:
+            repr_str += " at ({})".format(self.position_string()
+
+            )
+        except:
+            pass
+        return repr_str + ">"
+    
+    @property
+    def fixed_position(self):
+        """The position using :class:`astropy.coordinates.ICRS` in astrometric coordinates."""
+        return NotImplementedError("Can't be at a fixed position in AltAz")
+        
+    @property
+    def position(self):
+        """The position of this source in AltAz coordinates.."""
+        return self._position
+        
+    @position.setter
+    def position(self, value):
+        """Set the position attribute with an AltAz coordinate."""
+        self._position = AltAz(value)
+    
+@StarlistBase.register(FK4)
+@StarlistBase.register(FK5)
+class Target(FixedBody, StarlistBase):
+    """A subclass of FixedBody with a starlist interface."""
+    pass
     
 def parse_starlist_targets(starlist):
     """Parse a starlist into targets."""
