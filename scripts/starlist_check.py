e9a8270b3a77846f2a8a0efbedcd115cb337dee2
Alex Rudy <alex.rudy@gmail.com>
2014-09-10 17:23:51 -0700

    Script to check a starlist for validity

---
+++ scripts/starlist_check.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  starlist_check.py
+#  pyobserver
+#
+#  Created by Alexander Rudy on 2014-07-18.
+#  Copyright 2014 Alexander Rudy. All rights reserved.
+#
+
+import sys
+import os, os.path
+import argparse
+import six
+from pyobserver.starlist import (
+    read_skip_comments, verify_starlist_line, parse_starlist_line
+    )
+
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("starlist", type=six.text_type)
+    opt = parser.parse_args()
+    
+    print("Parsing starlist...")
+    for line_no, target_line in enumerate(read_skip_comments(opt.starlist)):
+        verify_starlist_line(target_line, "'{0}' line {1:d}".format(os.path.basename(opt.starlist), line_no))
+        print(parse_starlist_line(target_line))
